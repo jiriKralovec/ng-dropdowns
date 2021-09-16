e35106dd0ec6d301a4cb4c6adb66f49bd0933676
jiriKralovec <jrl@scancommerce.dk>
2021-09-16 08:38:32 +0200

    1 file modified

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -21,18 +21,26 @@ import {
     `
   ]
 })
-export class DropdownComponent {}
+export class DropdownComponent {
+  constructor() {}
+}
+export type AlignmentX = 'left' | 'right';
+export type AlignmentY = 'top' | 'bottom';
+export type AlignmentOrigin = `${AlignmentY}-${AlignmentX}`;
 
 @Directive({
   selector: '[floatingDropdown]'
 })
 export class DropdownDirective implements OnInit {
+  @Input('align-x') readonly alignX: AlignmentX;
+  @Input('align-y') readonly alignY: AlignmentX;
+  @Input('origin') readonly origin: AlignmentOrigin;
   @Input('relative-to') readonly relativeTo: HTMLElement;
   @Input('trigger') readonly _trigger: HTMLElement;
   private get trigger() {
     return this._trigger ?? this.relativeTo;
   }
-  constructor(private readonly host: ElementRef) {}
+  constructor(private readonly host: ElementRef<DropdownComponent>) {}
   ngOnInit() {
     console.log(this.trigger);
     console.log(this.host);
