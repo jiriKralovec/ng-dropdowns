e7277533661c085ebf5c035f4c260280d6acd3ab
jiriKralovec <jrl@scancommerce.dk>
2021-09-16 08:23:21 +0200

    4 files modified

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,49 @@
-import { Component, VERSION } from '@angular/core';
+import {
+  Component,
+  Directive,
+  ElementRef,
+  Input,
+  OnInit,
+  VERSION
+} from '@angular/core';
+
+@Component({
+  selector: 'ng-dropdown',
+  template: `
+    hey
+  `,
+  styles: [
+    `
+      :host {
+        display: block;
+        background: red;
+      }
+    `
+  ]
+})
+export class DropdownComponent {}
+
+@Directive({
+  selector: '[floatingDropdown]'
+})
+export class DropdownDirective implements OnInit {
+  @Input('relative-to') readonly relativeTo: HTMLElement;
+  @Input('trigger') readonly _trigger: HTMLElement;
+  private get trigger() {
+    return this._trigger ?? this.relativeTo;
+  }
+  constructor(private readonly host: ElementRef) {}
+  ngOnInit() {
+    console.log(this.trigger);
+    console.log(this.host);
+  }
+}
 
 @Component({
   selector: 'my-app',
   templateUrl: './app.component.html',
-  styleUrls: [ './app.component.css' ]
+  styleUrls: ['./app.component.css']
 })
-export class AppComponent  {
+export class AppComponent {
   name = 'Angular ' + VERSION.major;
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,15 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { FormsModule } from '@angular/forms';
 
-import { AppComponent } from './app.component';
-import { DrpCmp } from './dropdowns/dropdown-menu';
-import { DropdownDirective } from './dropdowns/dropdown-directive';
+import {
+  AppComponent,
+  DropdownComponent,
+  DropdownDirective
+} from './app.component';
 
 @NgModule({
   imports: [BrowserModule, FormsModule],
-  declarations: [AppComponent, DrpCmp, DropdownDirective],
+  declarations: [AppComponent, DropdownComponent, DropdownDirective],
   bootstrap: [AppComponent]
 })
 export class AppModule {}
